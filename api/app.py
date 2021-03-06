32451a92e4f913a9a4e218b467d969054a9cf06f
Cole Perry <perry391@marshall.edu>
2021-03-05 23:59:57 -0500

    Added flask gitignore

--- api/app.py
+++ api/app.py
@@ -1,9 +1,7 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mssql+pyodbc://cmp:cmpstg@T20DOHB05L03996\\SQLEXPRESS/outdoorsign?driver=ODBC Driver 17 for SQL Server"
-db = SQLAlchemy(app)
+
 
 @app.route('/api', methods=['GET'])
 def index():
