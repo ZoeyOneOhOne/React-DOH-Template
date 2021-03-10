ac096fa2c276597381588f0fda75af8a328955fb
Cole Perry <E096752@wv.gov>
2021-03-10 18:52:05 -0500

    May have made some progress on datbase

--- api/app.py
+++ api/app.py
@@ -1,19 +1,49 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from flask import jsonify
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mssql+pyodbc://cmp:cmpstg@T20DOHB05L03996\\SQLEXPRESS/outdoorsign?driver=ODBC Driver 17 for SQL Server"
+app.config['MAIL_SERVER'] = "10.200.146.27"
+app.config['MAIL_PORT'] = 25
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config["SQLALCHEMY_DATABASE_URI"] = create_engine("mssql+pyodbc://EXECUTIVE/DOHCovidAppSrv:!~&5HfTF4l@dotb6gisdbst01/Covid19?driver=ODBC+Driver+17+for+SQL+Server")
+# "mssql+pyodbc://EXECUTIVE/DOHCovidAppSrv:!~&5HfTF4l@dotb6gisdbst01/Covid19?driver=ODBC+Driver+17+for+SQL+Server?Trusted_Connection=yes"
+# sqlcon = create_engine("mssql+pyodbc://EXECUTIVE/DOHCovidAppSrv:!~&5HfTF4l@dotb6gisdbst01/Covid19?driver=ODBC+Driver+17+for+SQL+Server")
+#app.config["SQLALCHEMY_DATABASE_URI"] = "mssql+pyodbc://EXECUTIVE\\DOHCovidAppSrv:!~&5HfTF4l@dotb6gisdbst01/Covid19?driver=ODBC+Driver+17+for+SQL+Server?Trusted_Connection=yes"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mssql+pyodbc://localhost/Covid19?driver=ODBC+Driver+17+for+SQL+Server?trusted_connection=yes"
 db = SQLAlchemy(app)
+
+class Case_Log(db.Model):
+    ID = db.Column(db.Integer, primary_key=True)
+    Name = db.Column(db.String(50))
+    Phone1 = db.Column(db.String(50))
+    OrgNumber = db.Column(db.String(4))
+    DateOfTest = db.Column(db.String(50))
+    DateOfExposure = db.Column(db.String(50))
+    NumberOfExposed = db.Column(db.String(50))
+    Notes = db.Column(db.String(225))
+    PathToFile = db.Column(db.String(50))
+
+def case_log_serializer(Case_Log):
+    return{
+        'ID' : Case_Log.ID,
+        'Name' : Case_Log.Name
+    }
+
 
 @app.route('/api', methods=['GET'])
 def index():
-    return {
-        'name': 'What up!'
-    }
+    case_logs = Case_Log.query.all()
+    return jsonify([*map(case_log_serializer, Case_Log.query.all())])
+    # return {
+    #     'name': 'What up!'
+    # }
 
 @app.route('/<string:name>')
 def hello_world(name):
     return f'Hello, {name}'
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True);
